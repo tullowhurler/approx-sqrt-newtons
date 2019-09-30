df01c6c6dfe3f962084bf0e8c6929b2f5f16eb04
tullowhurler <G00364760@gmit.ie>
2019-09-30 21:58:09 +0100

    sntax error

--- approx-sqrt.py
+++ approx-sqrt.py
@@ -3,7 +3,7 @@ def sqrt(x):
     z = 1.0 
     # keep getting better estimate for the square root 
     # of x, until you are within two decimal places.
-    while abs(z*z - x) >= 0.01 
+    while abs(z*z - x) >= 0.01: 
         # get a better approximation for the square root. 
         z -= (z*z - x) / (2*z)
     # return z. 
