1ddd987bc910655c1ce06b22d6d6275a0e959ba1
tullowhurler <G00364760@gmit.ie>
2019-09-30 21:57:20 +0100

    Copying code

---
+++ approx-sqrt.py
@@ -0,0 +1,14 @@
+def sqrt(x):
+    # Intial guess 
+    z = 1.0 
+    # keep getting better estimate for the square root 
+    # of x, until you are within two decimal places.
+    while abs(z*z - x) >= 0.01 
+        # get a better approximation for the square root. 
+        z -= (z*z - x) / (2*z)
+    # return z. 
+    return z
+
+
+
+sqrt(8.0)
